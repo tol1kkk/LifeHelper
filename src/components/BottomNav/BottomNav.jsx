1f36f576d39f0b16800e0b375551933941f06eeb
Anatolii Semaniuk <toliksem123@gmail.com>
2026-05-30 00:28:21 +0100

    new design for navigation

--- src/components/BottomNav/BottomNav.jsx
+++ src/components/BottomNav/BottomNav.jsx
@@ -5,54 +5,49 @@ export default function BottomNav() {
   return (
     <nav className="nav">
 
-      <NavLink
-        to="/"
-        className={({ isActive }) =>
-          isActive ? "active" : "link"
-        }
-      >
-        <span>🏠</span>
-        <p>Home</p>
+      <NavLink to="/">
+        {({ isActive }) => (
+          <div className={isActive ? "menuItemActive" : "menuItem"}>
+         <img src={isActive ? "/homePurple.png" : "/homeGrey.png"} alt="Icon"/>
+        <p className={isActive ? "textActive" : "textDefault"}>Home</p>
+        </div>
+        )}
       </NavLink>
 
-      <NavLink
-        to="/tasks"
-        className={({ isActive }) =>
-          isActive ? "active" : "link"
-        }
-      >
-        <span>☑️</span>
-        <p>Tasks</p>
+      <NavLink to="/tasks">
+        {({ isActive }) => (
+          <div className={isActive ? "menuItemActive" : "menuItem"}>
+        <img src={isActive ? "/taskPurple.png" : "/taskGrey.png"} alt="Icon"/>
+        <p className={isActive ? "textActive" : "textDefault"}>Tasks</p>
+      </div>
+        )}
       </NavLink>
 
-      <NavLink
-        to="/budget"
-        className={({ isActive }) =>
-          isActive ? "active" : "link"
-        }
-      >
-        <span>💳</span>
-        <p>Budget</p>
+      <NavLink to="/budget">
+        {({ isActive }) => (
+          <div className={isActive ? "menuItemActive" : "menuItem"}>
+        <img src={isActive ? "/walletPurple.png" : "/walletGrey.png"} alt="Icon"/>
+        <p className={isActive ? "textActive" : "textDefault"}>Budget</p>
+      </div>
+        )}
       </NavLink>
 
-      <NavLink
-        to="/statistics"
-        className={({ isActive }) =>
-          isActive ? "active" : "link"
-        }
-      >
-        <span>📊</span>
-        <p>Statistics</p>
+      <NavLink to="/statistics">
+        {({ isActive }) => (
+          <div className={isActive ? "menuItemActive" : "menuItem"}>
+        <img src={isActive ? "/trendPurple.png" : "/trendGrey.png"} alt="Icon"/>
+        <p className={isActive ? "textActive" : "textDefault"}>Statistics</p>
+      </div>
+        )}
       </NavLink>
 
-      <NavLink
-        to="/profile"
-        className={({ isActive }) =>
-          isActive ? "active" : "link"
-        }
-      >
-        <span>👤</span>
-        <p>Profile</p>
+      <NavLink to="/profile">
+        {({ isActive }) => (
+          <div className={isActive ? "menuItemActive" : "menuItem"}>
+        <img src={isActive ? "/userPurple.png" : "/userGrey.png"} alt="Icon"/>
+        <p className={isActive ? "textActive" : "textDefault"}>Profile</p>
+      </div>
+        )}
       </NavLink>
 
     </nav>
